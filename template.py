8b28e137e5d8cce0cc1e7d9c74a7bda708f3c719
22948 <dava0086@gmail.com>
2025-02-25 00:06:25 +0800

    commit

--- template.py
+++ template.py
@@ -8,7 +8,9 @@ def get_strategy_template(name: str, desc: str):
 # DESCRIPTION={desc}
 
 
-prop = []
+prop = [
+    # ('index_name', lambda x: x.func())
+]
 
 
 def func(self):
@@ -16,6 +18,8 @@ def func(self):
     self.set_price_sell('high')
     for k, v in self.market.tell.items():
         # write your strategy here
+        # `self.li_buy.add(k)` for buy
+        # `self.li_sell.add(k)` for sell
         pass
 """
     return res
