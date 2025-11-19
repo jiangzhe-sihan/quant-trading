2d9666d4303cb0b48eef1df131f42f00cf295d08
22948 <dava0086@gmail.com>
2025-11-19 12:05:23 +0800

    commit

--- template.py
+++ template.py
@@ -17,9 +17,18 @@ def func(self):
     self.set_price_buy('low')
     self.set_price_sell('high')
     for k, v in self.market.tell.items():
-        # write your strategy here
+        # Write your strategy here
         # `self.li_buy.add(k)` for buy
         # `self.li_sell.add(k)` for sell
+        # Use series datas like this:
+        # ```
+        # if v.code not in self.static:
+        #     c = v.get_series('close')
+        #     cond = v.cross(c, v.ma(5, c))
+        #     self.static[v.code] = cond
+        # if self.static[v.code][v.date]:
+        #     self.li_buy.add(k)
+        # ```
         pass
 """
     return res
